9bb762058f6b8ec2c119428e62b9dcb6ede630ee
KIM <2016124068@kau.kr>
2022-07-13 02:42:10 +0900

    django course 27 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, DeleteView
+from django.views.generic import CreateView, DetailView, DeleteView, UpdateView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
@@ -25,23 +25,28 @@ from accountapp.models import HelloWorld
         # accountapp폴더의 urls.py에서 정한 app_name사용하고, urlpatterns리스트에서 path의 name으로 경로 갖고 오기
         # 이 때, reverse 함수를 사용하여 그 경로에 해당하는 경로를 다시 만든다
 def hello_world(request):
-    if request.method == "POST":
 
-        temp = request.POST.get('hello_world_input')
+    if request.user.is_authenticated:
+        if request.method == "POST":
 
-        #DB에 데이터 저장
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+            temp = request.POST.get('hello_world_input')
 
-        return HttpResponseRedirect(reverse("accountapp:hello_world"))
+            #DB에 데이터 저장
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
 
+            return HttpResponseRedirect(reverse("accountapp:hello_world"))
+
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html',
+                          context={'hello_world_list': hello_world_list})
     else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
-
+        return HttpResponseRedirect(reverse('accountapp:login'))
 ############################################################################################################
+
+
 class AccountCreateView(CreateView):
     # 1. 모델 선택
     # 2. User 모델을 만드는 데 필요한 form
@@ -53,23 +58,97 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
+
 class AccountDetailView(DetailView):
     model = User
     #context_object_name으로 template(detail.html)에서 사용하는 user의 이름을 다르게
-    #설정할 수 있다
+    #설정할 수 있다// template에서 사용할 context 변수명을 지정
     # 즉, 다른 사람의 내 페이지에 오더라도 내 정보를 볼 수 있음.
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-class AccountUpdateView(CreateView):
+    def get(self, *args, **kwargs):
+        #로그인이 되어 있다면, 변하는 게 없는 코드
+        if self.request.user.is_authenticated and \
+                self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        #로그인이 되어 있다면, 변하는 게 없는 코드
+        if self.request.user.is_authenticated and \
+                self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+
+        #Forbidden으로 금지된 곳에 접근했다고 알림
+        else:
+            return HttpResponseForbidden()
+
+
+class AccountUpdateView(UpdateView):
     model = User
+    context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
-    context_object_name = 'target_user'
+
+    #1. update창에서 logout하더라도, 나 포함 다른 users의 pw를
+    #바꿀 수 있다는 에러가 있었음
+    #2. 또한 test1로 접속해서 test1234의 계정을 지울 수 있다는 에러가
+    # 있음. 이를 방지하기 위해서, self.get_object()를 통해 class에서
+    # 현재 사용 중인 object(현재 user)를 갖고옴.
+    # 즉, url에서 현재의 pk에 해당하는 object를 갖고 옴.
+    # 그래서! 갖고 온 object가 현재(지금) requset를 보내고 있는 user와
+    # 동일한지를 확인함. // self.request.user
+
+    # --> self.get_object() == self.request.user를 통해 에러 방지.
+
+    def get(self, *args, **kwargs):
+        #로그인이 되어 있다면, 변하는 게 없는 코드
+        if self.request.user.is_authenticated and \
+                self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+
+        else:
+            return HttpResponseForbidden()
+
+
+    def post(self, *args, **kwargs):
+        #로그인이 되어 있다면, 변하는 게 없는 코드
+        if self.request.user.is_authenticated and \
+                self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+
+        else:
+            return HttpResponseForbidden()
+
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
-    context_object_name = 'target_user'
+
+    #delete창에서 logout하더라도, 나 포함 다른 users의 accounts를
+    #지울 수 있는 에러가 있었음
+
+    def get(self, *args, **kwargs):
+        #로그인이 되어 있다면, 변하는 게 없는 코드
+        if self.request.user.is_authenticated and \
+                self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        #로그인이 되어 있다면, 변하는 게 없는 코드
+        if self.request.user.is_authenticated and \
+                self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+
+        #Forbidden으로 금지된 곳에 접근했다고 알림
+        else:
+            return HttpResponseForbidden()
