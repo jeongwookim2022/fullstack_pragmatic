b6b13151d5b9e5da27ce13627e1eb2e3cd04d0ce
KIM <2016124068@kau.kr>
2022-07-28 21:58:45 +0900

    django course 60 commit

--- pragmatic/settings/base.py
+++ pragmatic/settings/base.py
@@ -13,35 +13,16 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import environ
 from pathlib import Path
-
 from django.urls import reverse_lazy
-
-env = environ.Env(
-    DEBUG=(bool, False)
-)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-#BASE 디렉토리: parent.parent : settings.py 기준으로 -> pragmatic -> pragmatic
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-#Reading .env file
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
+#BASE 디렉토리: parent.parent : base.py 기준으로 -> pragmatic -> pragmatic
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
