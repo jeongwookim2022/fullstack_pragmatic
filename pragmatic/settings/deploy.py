365f4efdce5cda9a2d51d61ec54b51c2bd10065c
KIM <2016124068@kau.kr>
2022-07-30 19:18:42 +0900

    Docker Secret

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -15,7 +15,21 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+
+# 처음에는 env 파일에서 갖고 오도록 서절을 했음 ------- (1)
+# Docker secrets를 사용하여 secret에서 갖고 오도록 다시 설정. -------- (2)
+# (1)
+# SECRET_KEY = env('SECRET_KEY')
+# (2)
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip() # secret_key 갖고 오면 양 옆에 불필요한 공백이 있음.
+    file.close()
+
+    return secret
+
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -31,7 +45,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'password1234',
+        # 'PASSWORD': 'password1234',
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
         'HOST': 'mariadb', #mariadb container 이름
         'PORT': '3306',
     }
